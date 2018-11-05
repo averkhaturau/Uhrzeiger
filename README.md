eaa9c6fbee43ff8ddfff8a53aac0c263750960ae
Aliaksei Verkhaturau <ext-aliaksei.verkhaturau@here.com>
2018-11-05 15:06:02 +0100

    simplify

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Uhrzeiger\n This repository contains coding test for determining positions of clock hands, depending on time
+# Uhrzeiger
+This repository contains coding test for determining positions of clock hands, depending on time
--- src/ClockHand.h
+++ src/ClockHand.h
@@ -3,27 +3,31 @@
 #include "base.h"
 
 #include <cmath>
+#include <functional>
 
 // Describes a base class  for a clock hand for the task
 class ClockHand
 {
 public:
-	//virtual ~ClockHand()=default;
-
-	int getAzimuth()const{return azimuth;}
-	//virtual setTime(time_t) = 0;
-protected:
-	void setAzimuth(int v){azimuth = v;};
+	ClockHand(std::function<int(time_t)> timeToAzimuth) : timeToAzimuthFn(timeToAzimuth) {}
+	int getAzimuth(time_t time)const{return timeToAzimuthFn(time);}
 private:
-	int azimuth = 0; // angle of the hand with upper posiiton
+	std::function<int(time_t)> timeToAzimuthFn;
 };
 
 // returns positive angle 0 .. 180
-int angle(const ClockHand& ch1, const ClockHand& ch2)
+int angle(const ClockHand& ch1, const ClockHand& ch2, time_t t)
 {
-	auto angle = std::abs(ch1.getAzimuth() - ch2.getAzimuth());
+	auto angle = std::abs(ch1.getAzimuth(t) - ch2.getAzimuth(t));
 	if (angle > numDegrees/2) {
 		angle = 360 - angle;
 	}
 	return angle;
 }
+
+
+const ClockHand hourHand([](time_t t){return t%(12*60*60)*numDegrees/(12*60*60);});
+
+const ClockHand minuteHand([](time_t t){return t%(60*60)*numDegrees/(60*60);});
+
+const ClockHand secondHand([](time_t t){return t%60*numDegrees/60;});
